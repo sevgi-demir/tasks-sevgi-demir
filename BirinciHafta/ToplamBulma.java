1eaecbdd337cdd1b318c14d353fda5ee4c3baef2
sevgi-demir <101891976+sevgi-demir@users.noreply.github.com>
2022-09-21 21:35:05 +0300

    Update ToplamBulma.java

--- BirinciHafta/ToplamBulma.java
+++ BirinciHafta/ToplamBulma.java
@@ -1,2 +1,23 @@
-package BirinciHafta;public class ToplamBulma {
+package BirinciHafta;
+
+/**
+ * 200 ile 500 arasındaki 3 ile tam bölünen sayılar dışındaki sayıların toplamını bulan program.
+ *
+ */
+
+public class ToplamBulma {
+    public static void main(String[] args) {
+        int toplam=0, sayac;
+        for(int i=200; i<500; i++) {
+            sayac=0;
+            if(i%3==0) {
+                sayac++;
+
+            }
+            if(sayac==0) {
+                toplam+=i;
+            }
+        }
+        System.out.println(toplam);
+    }
 }
